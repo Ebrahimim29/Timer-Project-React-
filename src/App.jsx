84b7745f9409652f158f2131919d86deba373a8a
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-12-22 08:38:26 +0330

    UseEffect-part2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  // const [time, setTime] = useState(new Date().toLocaleTimeString())
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString())
-    }, 1000)
+    if (time >= 10) {
+      return;
+    }
 
-    return () => clearInterval(interval)
-  }, [])
+    const interval = setInterval(() => {
+      // setTime(new Date().toLocaleTimeString())
+      setTime((prevTime) => {
+        if (prevTime >= 9) {
+          return 10;
+        }
+        return prevTime + 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+    // }, [])
+  }, [time]);
 
   return (
     <div className="main">
       <h1 className="title">Hello</h1>
-      <h2 className="timer">
-        it is {time}
-      </h2>
+      <h2 className="timer">it is {time}</h2>
     </div>
-  )
-}
+  );
+};
 
 export default App;
