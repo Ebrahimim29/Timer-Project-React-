92cb2f43694cab56d88fc5973d7dd0fb9b311d5d
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-12-23 07:31:32 +0330

    Props-part2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Timer from "./Timer";
 
 const App = () => {
-  // const [time, setTime] = useState(new Date().toLocaleTimeString())
-  const [time, setTime] = useState(0);
 
-  useEffect(() => {
-    if (time >= 10) {
-      return;
-    }
+  const [title , setTitle] = useState("Hello My Friend");
 
-    const interval = setInterval(() => {
-      // setTime(new Date().toLocaleTimeString())
-      setTime((prevTime) => {
-        if (prevTime >= 9) {
-          return 10;
-        }
-        return prevTime + 1;
-      });
-    }, 1000);
+  const handleSetTitle = () => {
+    setTitle("GoodBye My friend");
+  }
 
-    return () => clearInterval(interval);
-    // }, [])
-  }, [time]);
 
   return (
     <div className="main">
-      <h1 className="title">Hello</h1>
-      <h2 className="timer">it is {time}</h2>
+      <h1 className="title">{title}</h1>
+      <Timer handleSetTitle={handleSetTitle}/>
     </div>
   );
 };
