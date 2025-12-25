5ccfc682c11dd306e5445390262652f14fb01f7d
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-12-25 07:03:03 +0330

    ReactHooks-part4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import Timer from "./Timer";
 
+// Function Component:
 const App = () => {
-
   const [title , setTitle] = useState("Hello My Friend");
 
   const handleSetTitle = () => {
     setTitle("GoodBye My friend");
   }
-
 
   return (
     <div className="main">
@@ -17,5 +16,31 @@ const App = () => {
     </div>
   );
 };
+
+// Class Component:
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// class App extends React.Component{
+//   constructor() {
+//     super();
+//     this.state = {title:"Hello My Friend"}
+//   }  
+// }
+
+// handleSetTitle = () => {
+//   this.setState({
+//     title : "GoodBye My friend"
+//   })
+// }
+
+// render(){
+//   return(
+//     <div className="main">
+//       <Hello title={this.state.title}/>
+//       <Timer handleSetTitle={this.handleSetTitle}/>
+//     </div>
+//   )
+// }
 
 export default App;
