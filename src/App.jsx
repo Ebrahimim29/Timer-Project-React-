210500d1cfcb75a9755750ee17e2713200d4da5d
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-12-29 07:10:43 +0330

    Children-part 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Timer from "./Timer";
+import TimeList from "./TimeList";
 
 // Function Component:
 const App = () => {
   const [title , setTitle] = useState("Hello My Friend");
   const [isLight, setIsLight] = useState(false);
+  const [timeArr, setTimeArr] = useState(["00 : 05 : 12", "01 : 10 :30"]);
+
+  useEffect(() => {
+    console.log("useEffect");
+    return() => {
+
+    }
+  },[isLight])
 
   const handleSetTitle = () => {
     setTitle("GoodBye My friend");
@@ -18,7 +27,11 @@ const App = () => {
     <div className="main" style={{background:isLight ? "white" : "black"}}>
       <h1 className="title">{title}</h1>        
       <Timer handleSetTitle={handleSetTitle} isLight={isLight} handleSetIsLight={handleSetIsLight}/>      
-    </div>
+      {/* <TimeList timeArr={timeArr} /> */}
+      <TimeList>
+        {timeArr}
+      </TimeList>
+    </div>    
   );
 };
 
